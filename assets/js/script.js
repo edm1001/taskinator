810dbed213e991af7eb421e9cbe6aa28170aeda7
Edmer Valencia <edmer_franciz@yahoo.com>
2022-04-09 15:11:02 -0500

    handle form submissions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-var taskItemEl=document.createElement("li");//DOM
-taskItemEl.textContent=("hello");//DOM
-tasksToDoEl.appendChild(taskItemEl);
-taskItemEl.className = "task-item";
-
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
   event.preventDefault();
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+  var taskNameInput = document.querySelector("input[name='task-name']").value;//(input[class=tag]) allows targeting of HTML & allows user to enter whatever
+  var taskTypeInput= document.querySelector("select[name='task-type']").value//targets the scrolldown for task type.
+    
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
   };
   formEl.addEventListener("submit", createTaskHandler);
   //adds new task on tasks to do
